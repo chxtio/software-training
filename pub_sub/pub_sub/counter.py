5fe2f336bc12db188c6650960335d26f7317ea72
chxtio <chxtio@gmail.com>
2023-11-23 22:47:01 -0800

    Add publisher

--- pub_sub/pub_sub/counter.py
+++ pub_sub/pub_sub/counter.py
@@ -1,11 +1,18 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Int32
 
 class CounterNode(Node):
     def __init__(self):
         super().__init__('counter')
 
     self.counter = 0
+
+    self.pub = self.create_publisher(
+        Int32,
+        '/count',
+        10
+    )
 
     self.create_timer(
         1.0,
@@ -14,6 +21,10 @@ class CounterNode(Node):
 
     def execute(self):
         self.counter += 1
+
+        msg = Int32()
+        msg.data = 'Hello World: %d' % self.counter
+        self.pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
